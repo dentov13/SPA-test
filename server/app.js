95cad34461b09d3cd5097aaa04231a45a216bcce
dentov13 <tovstokhatko.denis@gmail.com>
2017-02-03 20:59:25 +0200

    Get method

--- server/app.js
+++ server/app.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import * as db from './utils/DataBaseUtils.js';
+
+db.setUpConnection();
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+
+app.get('/notes', (req, res) => {
+  db.listNotes().then(data => res.send(data));
+});
+
+app.post('/notes', (req, res) => {
   res.send('Hey, sup!');
-})
+});
+
+app.delete('/notes/:id', (req, res) => {
+  res.send('Hey, sup!');
+});
 
 const server = app.listen(8080, () => {
   console.log(`Server is UP on port 8080`);
