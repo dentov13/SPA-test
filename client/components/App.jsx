178551d8ebbada6d1b0921ad78613d41731faae9
dentov13 <tovstokhatko.denis@gmail.com>
2017-02-08 00:13:20 +0200

    Add components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import NoteEditor from './NoteEditor.jsx'
+import NotesGrid from './NotesGrid.jsx'
+
 const App = React.createClass({
   render() {
-    return <h1> :) </h1>;
+    return (
+      <div className='App'>
+        <h1 className='App__header'>NotesApp</h1>
+        <NoteEditor />
+        <NotesGrid />
+      </div>
+    );
   }
 });
 
---
+++ client/components/NoteEditor.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NoteEditor = React.createClass({
+  render() {
+    return <h2> NoteEditor </h2>;
+  }
+});
+
+export default NoteEditor;
