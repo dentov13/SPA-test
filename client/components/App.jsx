b042dfa7f83123f4b13bd0d295e2a3ff7e5aed31
dentov13 <tovstokhatko.denis@gmail.com>
2017-02-12 14:24:35 +0200

    NoteEditor component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import NoteEditor from './NoteEditor.jsx'
-import NotesGrid from './NotesGrid.jsx'
+import NoteEditor from './NoteEditor.jsx';
+import NotesGrid from './NotesGrid.jsx';
 
 const App = React.createClass({
+  handleNoteAdd(data) {
+    console.log(data);
+  },
   render() {
     return (
       <div className='App'>
         <h1 className='App__header'>NotesApp</h1>
-        <NoteEditor />
+        <NoteEditor onNoteAdd={this.handleNoteAdd}/>
         <NotesGrid />
       </div>
     );
--- client/components/NoteEditor.jsx
+++ client/components/NoteEditor.jsx
@@ -1,8 +1,58 @@
 import React from 'react';
+import './NoteEditor.less';
 
 const NoteEditor = React.createClass({
+  getInitialState() {
+    return {
+      title: '',
+      text: '',
+      color: '#FFFFFF'
+    };
+  },
+  handleTextChange(e) {
+    this.setState({ text: e.target.value });
+  },
+  handleTitleChange(e) {
+    this.SetState({ title: e.target.value });
+  },
+  handleNoteAdd() {
+    const newNode = {
+      title: this.state.title,
+      text: this.state.text,
+      color: this.state.color
+    };
+
+    this.props.onNoteAdd(newNote);
+    this.setState({ text: '', title: '', color: '#FFFFFF' });
+  },
   render() {
-    return <h2> NoteEditor </h2>;
+    return (
+      <div class='NoteEditor'>
+        <input
+          text='text'
+          className='NoteEditor__title'
+          placeholder='Enter title'
+          value={this.state.title}
+          onChange={this.handleTitleChange}
+        />
+        <textarea
+          className='NoteEditor__text'
+          placeholder='Enter note text'
+          rows={5}
+          value={this.state.text}
+          onChange={this.handleTextChange}
+        />
+        <div className='NoteEditor__footer'>
+          <button
+            className='NoteEditor__button'
+            disabled={!this.state.text}
+            onClick={this.handleNoteAdd}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    );
   }
 });
 
